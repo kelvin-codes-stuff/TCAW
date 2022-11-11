1aaca8b54b79224bd8116f887fe9b44cdbb0e44e
Kelvin de Reus <k_dereus@outlook.com>
2022-11-11 23:27:22 +0100

    Updates(saterday next job haha)

--- TheCatApiWrapper.py
+++ TheCatApiWrapper.py
@@ -4,7 +4,12 @@ import requests_cache
 from dotenv import load_dotenv
 import os
 
-#TODO: Make code more flexible.
+#TODO: Make own caching code
+#TODO: Remove URL joining
+#TODO: Use Exceoption instead of "return "Error""
+#TODO: More information inside comments
+#TODO: Make code more flexible
+#TODO: Make handling_url more efficient
 
 # Loads env values
 load_dotenv()
@@ -20,7 +25,7 @@ HEADERS = {f"x-api-key": "{API_KEY}"}
 
 # URL collection, callable with Url_join
 class Url_join():
-    imagesQuestionmark = "images/search?"
+    images_questionmark = "images/search?"
     images_get_breed = "images/search?breed_ids="
     images_search = "images/search"
     show_vote_limit10 = "votes?limit=10&order=DESC"
@@ -31,39 +36,43 @@ class Url_join():
 # Main class to access all different functions
 class Tcaw:
     # Gets a random image
-    def getRandomImage():
+    def get_random_image():
         # Making the request with joined URL and header for the API KEY
-        getRequest = requests.get(BASE_API_URL + Url_join.imagesQuestionmark, headers=HEADERS)
+        get_request = requests.get(BASE_API_URL + Url_join.images_questionmark, headers=HEADERS)
         # Execute handling_url while passing getRequest trough the function, and returning the value from handling_url to getRandomImage
-        return handling_url(getRequest)
+        return handling_url(get_request)
 
     # Gets a image from the requested cat breed
-    def GetBreedImage(breed):
+    def get_breed_image(breed_input):
         # Request all breeds, if breed is in the request, get the id from that breed
-        get_breeds_request = requests.get("https://api.thecatapi.com/v1/breeds").json()
-        for key in get_breeds_request:
+        for key in requests.get("https://api.thecatapi.com/v1/breeds").json():
             # if key == breed input, add id from that breed to cat_id
-            if key["name"].lower() == breed.lower():
-                cat_id = key["id"]
+            if key["name"].lower() == breed_input.lower():
+                
                 # Making the request with joined URL, cat breed and header for the API KEY
-                getRequest = requests.get(BASE_API_URL + Url_join.images_get_breed + cat_id, headers=HEADERS)
+                get_request = requests.get(BASE_API_URL + Url_join.images_get_breed + key["id"], headers=HEADERS)
                 # Execute handling_url while passing getRequest trough the function, and returning the value from handling_url to getRandomImage
-                return handling_url(getRequest)
-
+                return handling_url(get_request)
+            
         return "Error, unknown cat breed!"
 
-
-# Gets executes with each API request
-def handling_url(getRequest):
-        # If status code isn't 200, return "Error" to caller
-        if getRequest.status_code != 200:
-            return "Error, got a invalid respone!"
-        # If everything is fine, make JSON from request, extract URL and return URL to caller
-        response = getRequest.json()
+    # Get information about a breed
+    def get_breed_info(breed_input):
+        # Request all breeds, and loop for every key in request
+        get_breeds_request = requests.get("https://api.thecatapi.com/v1/breeds").json()
+        for key in get_breeds_request:
+            # if key == breed input, return list with information
+            if key["name"].lower() == breed_input.lower():
+                    return key
+                
         
+# Gets executes with each API request
+def handling_url(get_request):
+        # If status code isn't 200, return "Error" to caller
+        if get_request.status_code != 200:
+            return "Error, got a invalid respone!"
         # If result isn't nothing
-        if response != []:
-            url_extract =  response[0]["url"]
-            return url_extract, f"Used cache: {getRequest.from_cache}"
+        if get_request.json() != []:
+            return get_request.json()[0]["url"]
         # If result is nothing return "No result"
         return "Error, no results!"        
