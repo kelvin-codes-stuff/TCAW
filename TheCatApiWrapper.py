6cf11c2fbc70d30a14098349d787242fc9478e74
Kelvin de Reus <k_dereus@outlook.com>
2022-11-11 19:50:29 +0100

    Update code

--- TheCatApiWrapper.py
+++ TheCatApiWrapper.py
@@ -1,15 +1,18 @@
 # Getting the required modules
 import requests
+import requests_cache
 from dotenv import load_dotenv
 import os
-# Loads env 
+
+# Loads env values
 load_dotenv()
 
-# Base URL and API key stuff
+# Base URL and API key stuff, adding header to request.
 BASE_API_URL = "https://api.thecatapi.com/v1/"
 API_KEY = os.getenv("API_KEY")
 HEADERS = {f"x-api-key": "{API_KEY}"}
 
+# URL collection, callable with Url_join
 class Url_join():
     imagesQuestionmark = "images/search?"
     images_get_breed = "images/search?breed_ids="
@@ -17,35 +20,34 @@ class Url_join():
     show_vote_limit10 = "votes?limit=10&order=DESC"
     show_favourites = "favourites"
 
-    
-# Main class to access all different functions
-class Tcaw:
-    
-    # Getting a random image function
-    def getImage():
-        # Making the request with joined URL
-        get_img = requests.get(BASE_API_URL + Url_join.imagesQuestionmark, headers=HEADERS)
-        
-        # If shit goes wrong af, return a error
-        if get_img.status_code != 200:
-            return "Error"    
-        
-        # If shit goes good, extract URL and return URL
-        response = get_img.json()
-        url_extract = response[0]["url"]
-        return url_extract
-
-
-    # Get a image from the requested cat breed
-    def get_breed_img(breed):
-        getRequest = requests.get(BASE_API_URL + Url_join.images_get_breed + breed, headers=HEADERS)
-        
+# Gets executes with each API request.
+def handling_url(getRequest):
+        # If status code isn't 200, return "Error" to caller
         if getRequest.status_code != 200:
             return "Error"
-
+        # If status code = 200, but result = empty return "No result" to caller
         if getRequest == []:
             return "No result"
-
+        # If everything is fine, make JSON from request, extract URL and return URL to caller
         response = getRequest.json()
         url_extract =  response[0]["url"]
         return url_extract        
+
+
+# Main class to access all different functions
+class Tcaw:
+    
+    # Gets a random image
+    def getRandomImage():
+        # Making the request with joined URL
+        getRequest = requests.get(BASE_API_URL + Url_join.imagesQuestionmark, headers=HEADERS)
+        # Execute handling_url while passing getRequest trough the function, and returning the value from handling_url to getRandomImage
+        return handling_url(getRequest)
+
+    # Gets a image from the requested cat breed
+    def GetBreedImage(breed):
+        getRequest = requests.get(BASE_API_URL + Url_join.images_get_breed + breed, headers=HEADERS)
+        # Execute handling_url while passing getRequest trough the function, and returning the value from handling_url to getRandomImage
+        return handling_url(getRequest)
+    
+
