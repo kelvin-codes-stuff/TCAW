a61797e504e7ee1336d06b31db744dd07bce3eef
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2022-11-18 19:50:39 +0100

    Added cache code

--- TheCatApiWrapper.py
+++ TheCatApiWrapper.py
@@ -1,12 +1,12 @@
 # Getting the required modules
 import requests
-import requests_cache
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+
 
 #TODO: Make own caching code
-#TODO: Remove URL joining
-#TODO: Use Exceoption instead of "return "Error""
+#TODO: Use Exception instead of "return "Error"
 #TODO: More information inside comments
 #TODO: Make code more flexible
 #TODO: Make handling_url more efficient
@@ -14,31 +14,46 @@ import os
 # Loads env values
 load_dotenv()
 
-# Caching (Only if .env CACHE is true!)
-if os.getenv("CACHE") == "True":
-    requests_cache.install_cache(cache_name='URL_caching', backend='sqlite', expire_after=180)
-
 # Base URL and API key stuff, adding header to request.
 BASE_API_URL = "https://api.thecatapi.com/v1/"
 API_KEY = os.getenv("API_KEY")
 HEADERS = {f"x-api-key": "{API_KEY}"}
 
-# URL collection, callable with Url_join
-class Url_join():
-    images_questionmark = "images/search?"
-    images_get_breed = "images/search?breed_ids="
-    images_search = "images/search"
-    show_vote_limit10 = "votes?limit=10&order=DESC"
-    show_favourites = "favourites"
 
+# Cache module
+cache_files = {}
+
+def write_cache(breed, breedkey, img_url):
+    now = datetime.now()
+    cache_files[breed] = {
+        "breed_id": breedkey,
+        "expire": int(now.strftime("%H%M%S")) + 10,
+        "image_url": img_url
+    }
+    return cache_files[breed]["image_url"]
+
+def read_cache(breed):
+    if not (cache_files.get(breed) is None):
+        return cache_files[breed]["image_url"]
+    return None
+
+def check_cache(breed):
+    now = datetime.now()
+
+    cache_file_expire = cache_files[breed]["expire"]
+    if int(now.strftime("%H%M%S")) > cache_file_expire:
+        return None
+    else:
+        return "IDK"
 
 
 # Main class to access all different functions
 class Tcaw:
+
     # Gets a random image
     def get_random_image():
         # Making the request with joined URL and header for the API KEY
-        get_request = requests.get(BASE_API_URL + Url_join.images_questionmark, headers=HEADERS)
+        get_request = requests.get(BASE_API_URL + "images/search?", headers=HEADERS)
         # Execute handling_url while passing getRequest trough the function, and returning the value from handling_url to getRandomImage
         return handling_url(get_request)
 
@@ -46,25 +61,28 @@ class Tcaw:
     def get_breed_image(breed_input):
         # Request all breeds, if breed is in the request, get the id from that breed
         for key in requests.get("https://api.thecatapi.com/v1/breeds").json():
-            # if key == breed input, add id from that breed to cat_id
+            # if key is breed input, add id from that breed to cat_id
             if key["name"].lower() == breed_input.lower():
                 
                 # Making the request with joined URL, cat breed and header for the API KEY
-                get_request = requests.get(BASE_API_URL + Url_join.images_get_breed + key["id"], headers=HEADERS)
-                # Execute handling_url while passing getRequest trough the function, and returning the value from handling_url to getRandomImage
-                return handling_url(get_request)
-            
-        return "Error, unknown cat breed!"
+                get_request = requests.get(BASE_API_URL + "images/search?" + key["id"], headers=HEADERS)
 
-    # Get information about a breed
-    def get_breed_info(breed_input):
-        # Request all breeds, and loop for every key in request
-        get_breeds_request = requests.get("https://api.thecatapi.com/v1/breeds").json()
-        for key in get_breeds_request:
-            # if key == breed input, return list with information
-            if key["name"].lower() == breed_input.lower():
-                    return key
+                if get_request.status_code != 200:
+                    return "Error, got a invalid respone!"
+
+                if read_cache(breed_input) == None:   
+                    print("DEBUG, writing to cache")
+                    return write_cache(breed=breed_input, breedkey=key["id"], img_url=get_request.json()[0]["url"])
+
+                if check_cache(breed=breed_input) == None:
+                    print("DEBUG, expired, writing new to cache")
+                    return write_cache(breed=breed_input, breedkey=key["id"], img_url=get_request.json()[0]["url"])
                 
+                if check_cache != None:
+                    print("DEBUG, reading from cache")
+                    return read_cache(breed=breed_input)
+
+
         
 # Gets executes with each API request
 def handling_url(get_request):
@@ -76,3 +94,20 @@ def handling_url(get_request):
             return get_request.json()[0]["url"]
         # If result is nothing return "No result"
         return "Error, no results!"        
+
+
+
+# UI
+while True:
+    # Getting image of a cat breed
+    input_breed_user = input(">").lower()
+
+    if input_breed_user == "exit":
+        break
+
+    if input_breed_user == "random":
+        print(Tcaw.get_random_image())        
+    else:
+        print(Tcaw.get_breed_image(input_breed_user))
+
+
